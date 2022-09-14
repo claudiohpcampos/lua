fda0e289d7e3a558387702d76f0deb5476606a2e
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-13 23:25:55 -0300

    Create aula02.lua

---
+++ aula02/aula02.lua
@@ -0,0 +1,16 @@
+nome = "Claudio"
+num1 = 10
+num2 = 5
+valor = 5.2
+logica = true
+vazia = nil
+
+soma = num1 + num2
+print('Soma de',num1,'com',num2,'e igual a',soma)
+
+print('Tipo:', type(nome) , 'valor:', nome)
+print('Tipo:', type(num1) , 'valor:', num1)
+print('Tipo:', type(valor) , 'valor:', valor)
+print('Tipo:', type(logica) , 'valor:', logica)
+print('Tipo:', type(vazia) , 'valor:', vazia)
+print(type(type))
